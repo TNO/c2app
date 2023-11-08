4b5571603b81bd57a057784cc8250c707746fc24
Erik Vullings <erik.vullings@tno.nl>
2023-11-08 11:27:27 +0100

    Use layerStyle.ui for the poi sidebar

--- packages/gui/src/components/sidebars/poi-sidebar.ts
+++ packages/gui/src/components/sidebars/poi-sidebar.ts
@@ -1,73 +1,78 @@
 import m from 'mithril';
 import { MeiosisComponent } from '../../services/meiosis';
-import {
-  formatMan,
-  formatCar,
-  alertFormatComponent,
-  resourceFormatComponent,
-  sensorFormatComponent,
-  formatUnknown,
-  incidentLocationFormatComponent,
-  contextFormatComponent,
-} from './poi-formatting';
 import { FlatButton } from 'mithril-materialized';
+import { toSourceName } from '../map/map-utils';
+import { FormAttributes, LayoutForm, UIForm } from 'mithril-ui-form';
 
 export const poiSidebar: MeiosisComponent = () => {
   return {
     view: ({ attrs: { state, actions } }) => {
       const {
-        app: { clickedFeature, sensorDict },
+        app: { clickedFeature, sources },
       } = state;
+
+      const sourceName = clickedFeature && clickedFeature.source;
+      const source = sources.find((s) => toSourceName(s) === sourceName);
+      console.log(source)
+      const shared = (source && source.shared) || false;
+      const ui = (source && source.ui) || ([] as UIForm);
+      const form =
+        clickedFeature &&
+        (shared
+          ? ui
+          : ui.filter(
+              ({ id = '' }) =>
+                !id ||
+                (clickedFeature.properties.hasOwnProperty(id) && typeof clickedFeature.properties[id] !== 'undefined')
+            ));
+
       return m(
         'ul#slide-out-2.sidenav.no-autoinit',
-        m('.row', [m('.col.s12',
-          m(FlatButton, {
-            className: 'right',
-            iconName: 'clear',
-            onclick: () => {
-              const elem = document.getElementById('slide-out-2') as HTMLElement;
-              M.Sidenav.getInstance(elem).close();
-              actions.resetClickedFeature();
-            }
-          })),
-        m('h5', 'Details'),
-        // If there is a clicked feature
-        m('.col.s12', [
-          clickedFeature
-            ? clickedFeature?.properties?.type === 'resource'
-              ? m(resourceFormatComponent, { state, actions })
-              : clickedFeature?.properties?.type === 'sensor'
-                ? m(sensorFormatComponent, { state, actions })
-                : clickedFeature?.properties?.type === 'context'
-                  ? m(contextFormatComponent, { state, actions })
-                  : clickedFeature?.properties?.type === 'plume'
-                    ? m(alertFormatComponent, { state, actions })
-                    : clickedFeature?.properties?.type === 'incidentLocation'
-                      ? m(incidentLocationFormatComponent, { state, actions })
-                      : clickedFeature?.properties?.type === 'MAN'
-                        ? formatMan(clickedFeature)
-                        : clickedFeature?.properties?.type === 'CAR'
-                          ? formatCar(clickedFeature)
-                          : clickedFeature?.properties?.type === 'FIREFIGHTER'
-                            ? formatMan(clickedFeature)
-                            : formatUnknown(clickedFeature)
-            : // If there is no clicked feature
-            m('p', ''),
-        ]),
-        m('p', sensorDict),
-        m('div', { style: 'margin: 128px' }),
+        m('.row', [
+          m(
+            '.col.s12',
+            m(FlatButton, {
+              className: 'right',
+              iconName: 'clear',
+              onclick: () => {
+                const elem = document.getElementById('slide-out-2') as HTMLElement;
+                elem && M.Sidenav.getInstance(elem).close();
+                actions.resetClickedFeature();
+              },
+            })
+          ),
+          m(
+            '.col.s12',
+            form && [
+              m(LayoutForm, {
+                form,
+                obj: clickedFeature.properties,
+                onchange: async () => {
+                  if (source && source.source && source.source.features) {
+                    console.log(clickedFeature);
+                    const { id } = clickedFeature.properties;
+                    source.source.features = source.source.features.map(f => f.properties?.id === id ? clickedFeature : f);
+                    await actions.saveSource(source)
+                  }
+                  
+                  // actions.updateClickedFeature(clickedFeature);
+                },
+              } as FormAttributes),
+            ]
+          ),
         ])
       );
     },
     oncreate: () => {
       const elem = document.getElementById('slide-out-2') as HTMLElement;
-      M.Sidenav.init(elem, {
-        edge: 'right',
-        onOpenStart: function (elem: Element) {
-          // @ts-ignore
-          elem.M_Sidenav._overlay.remove();
-        },
-      });
+      elem &&
+        M.Sidenav.init(elem, {
+          edge: 'right',
+          onOpenStart: function (_elem: Element) {
+            // @ts-ignore
+            // elem.M_Sidenav._overlay.remove();
+          },
+        });
     },
   };
 };
